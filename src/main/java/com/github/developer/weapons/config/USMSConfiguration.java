ce511f1cc28d38680ab68b918f15ff54f156e85a
codedrinker <gwangchunlei@gmail.com>
2020-04-05 12:57:41 +0800

    添加 bean

--- src/main/java/com/github/developer/weapons/config/USMSConfiguration.java
+++ src/main/java/com/github/developer/weapons/config/USMSConfiguration.java
@@ -1,19 +1,24 @@
 package com.github.developer.weapons.config;
 
+import com.github.developer.weapons.service.UFileService;
 import com.github.developer.weapons.service.USMSService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(USMSService.class)
-@EnableConfigurationProperties(USMSProperties.class)
 public class USMSConfiguration {
     @Bean
     @ConditionalOnMissingBean
-    public USMSService usmsService() {
+    public USMSService uSmsService() {
         return new USMSService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UFileService uFileService() {
+        return new UFileService();
     }
 }
