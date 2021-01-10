75e3037a4578c5dd28f596af899345d15e27e482
codedrinker <gwangchunlei@gmail.com>
2021-01-10 11:38:34 +0800

    增加图片支持

--- src/main/java/com/github/developer/weapons/utils/FileUtils.java
+++ src/main/java/com/github/developer/weapons/utils/FileUtils.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -13,18 +15,39 @@ import java.util.UUID;
  */
 @Slf4j
 public class FileUtils {
+    private static List<String> suffix = new ArrayList<>();
+
+    static {
+        suffix.add(".png");
+        suffix.add(".bmp");
+        suffix.add(".gif");
+        suffix.add(".jpeg");
+        suffix.add(".jpg");
+    }
+
+    public static boolean hasSuffix(String fileName) {
+        for (String s : suffix) {
+            if (StringUtils.endsWithIgnoreCase(fileName, s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static String newUUIDFileName(String fileName) {
         if (StringUtils.isBlank(fileName)) {
             return fileName;
         }
-        String generatedFileName;
-        String[] filePaths = fileName.split("\\.");
-        if (filePaths.length > 1) {
-            generatedFileName = UUID.randomUUID().toString().replace("-", "") + "." + filePaths[filePaths.length - 1];
+        if (hasSuffix(fileName)) {
+            String[] filePaths = fileName.split("\\.");
+            if (filePaths.length > 1) {
+                return UUID.randomUUID().toString().replace("-", "") + "." + filePaths[filePaths.length - 1];
+            } else {
+                return newUUIDPNGFileName();
+            }
         } else {
             return newUUIDPNGFileName();
         }
-        return generatedFileName;
     }
 
     public static String newUUIDPNGFileName() {
@@ -47,8 +70,8 @@ public class FileUtils {
         file = new File(localFile);
         try {
             org.apache.commons.io.FileUtils.copyURLToFile(new URL(url), file);
-        } catch (IOException e) {
-            log.error("FILE_UTILS_NEW_ERROR, url : {}", url);
+        } catch (Exception e) {
+            log.error("FILE_UTILS_NEW_ERROR, url : {}", url, e);
             return null;
         }
         return file;
